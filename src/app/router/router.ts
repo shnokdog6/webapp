0f51aaa92b5f211ec806b8b9ff7437caead33965
beyondray <shnokdog123@gmail.com>
2024-08-18 11:54:10 +0500

    update

--- src/app/router/router.ts
+++ src/app/router/router.ts
@@ -1,11 +1,10 @@
-import { createRouter, createWebHistory } from "vue-router";
 import type { RouteRecordRaw } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 import { MainPage } from "@pages/main";
-
 
 const routes: RouteRecordRaw[] = [{ path: "/", component: MainPage }];
 
 export const router = createRouter({
-    history: createWebHistory(),
+    history: createWebHashHistory(),
     routes,
 });
